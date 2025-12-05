6692d834c3abe3326b130429d2264cac3d4ad5be
Himansi Shrestha <shresthahimansi@gmail.com>
2025-12-05 23:17:37 +0545

    Changed code

--- lib/views/entrypoint.dart
+++ lib/views/entrypoint.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:foodapp/constants/constants.dart';
+import 'package:foodapp/controllers/tab_index_controller.dart';
+import 'package:get/get.dart';
+
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller =Get.put(TabIndexController());
     return Scaffold(
       body: Stack(     
         children:[
@@ -18,6 +22,13 @@ class MainScreen extends StatelessWidget {
             alignment: Alignment.bottomCenter,
             child: Theme(data: Theme.of(context).copyWith(canvasColor: kPrimary), 
             child: BottomNavigationBar(
+              showSelectedLabels: false,
+              showUnselectedLabels: false,
+              unselectedIconTheme: const IconThemeData(color: Colors.black38),
+              selectedIconTheme: const IconThemeData(color: kSecondary),
+              onTap: (value){
+                print(value);
+              },
               items: const [
                 BottomNavigationBarItem(icon: Icon(Icons.home), label:'Home'),
                 BottomNavigationBarItem(icon: Icon(Icons.search), label:'Search'),
