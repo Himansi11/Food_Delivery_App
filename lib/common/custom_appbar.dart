2debed736f6ca7ee7b7657fa6d19a995962c8140
Himansi Shrestha <shresthahimansi@gmail.com>
2025-12-08 21:38:11 +0545

    Changed code

--- lib/common/custom_appbar.dart
+++ lib/common/custom_appbar.dart
@@ -4,6 +4,7 @@ import 'package:foodapp/common/app_style.dart';
 import 'package:foodapp/common/reusable_text.dart';
 import 'package:foodapp/constants/constants.dart';
 
+
 class CustomAppbar extends StatelessWidget {
   const CustomAppbar({super.key});
 
@@ -15,24 +16,59 @@ class CustomAppbar extends StatelessWidget {
       width: width,
       color: kOffwhite,
       child: Container(
+        margin: EdgeInsets.only(top:20.h),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.end,
           children: [
-            CircleAvatar(),
-            Padding(
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+              CircleAvatar(
+                radius: 22.r,
+                backgroundColor: kSecondary,
+                backgroundImage: const AssetImage('assets/profile1.jpg'),
+              ),
+             Padding(
               padding: const EdgeInsets.only(bottom: 6, left: 8),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.end,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  ReusableText(text:"Deliver to", style: appStyle(13, kSecondary, FontWeight.w600)),
-                  Text("16768 21st Ave N, Plymouth, MN 55447", style: appStyle(13, kDark, FontWeight.w400),)
+                  ReusableText(text:"Deliver to", 
+                  style: appStyle(13, kSecondary, FontWeight.w600)),
+                  SizedBox(
+                    width: width*0.65,
+                    child: Text("16768 21st Ave N, Plymouth, MN 55447",
+                    overflow: TextOverflow.ellipsis,
+                    style: appStyle(11, kGrayLight, FontWeight.normal),),
+                  ),
                 ],
               ),
-            )
+            ),
+
           ],
         ),
+          Text(
+          getTimeofDay(),
+          style: const TextStyle(fontSize: 35),
+          )
+       ]
       ),
+      )
     );
+  }
+  String getTimeofDay(){
+    DateTime now=DateTime.now();
+    int hour=now.hour;
+
+    if(hour>=0 && hour<12){
+      return ' ☀️ ';
+    }else if(hour>=12 && hour<16){
+      return ' ⛅ ';
+    }
+    else{
+      return ' 🌙 ';
+    }
   }
 }
--- lib/views/home/home_page.dart
+++ lib/views/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:foodapp/common/custom_appbar.dart';
 import 'package:foodapp/common/custom_conatiner.dart';
 import 'package:foodapp/constants/constants.dart';
 
@@ -10,10 +11,10 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kPrimary,
-      appBar: PreferredSize(preferredSize: Size.fromHeight(130.h), 
-      child: Container(
-        height: 130,
-      )),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(130.h), 
+        child: const CustomAppbar()
+       ),
       body: SafeArea(
         child: CustomConatiner(containerContent: Container())
       ),
