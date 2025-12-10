7fd83bab0d4e5af0ce5d0d5ed57dc0b08530798e
Himansi Shrestha <shresthahimansi@gmail.com>
2025-12-10 22:12:59 +0545

    Code CHanged

--- lib/views/categories/all_categories.dart
+++ lib/views/categories/all_categories.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:foodapp/common/app_style.dart';
+import 'package:foodapp/common/reusable_text.dart';
+import 'package:foodapp/constants/constants.dart';
+import 'package:foodapp/constants/uidata.dart';
 
 class AllCategories extends StatelessWidget {
   const AllCategories({super.key});
@@ -7,8 +12,29 @@ class AllCategories extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('All Categories'),
-      )
+        elevation: 0,
+        backgroundColor: kOffwhite,
+        title: ReusableText(text: "All Categories",
+         style: appStyle(12, kGrey, FontWeight.w600)),
+      ),
+
+      body: Container(
+        padding: EdgeInsets.only(left: 12.w, top: 10.h),
+        height: height,
+        child: ListView(
+      scrollDirection: Axis.vertical,
+      children: List.generate(categories.length, (i){
+        var category = categories[i];
+        return ListTile(
+          leading: CircleAvatar(
+            radius: 18.r,
+            backgroundColor: kGrayLight,
+            backgroundImage: NetworkImage(category["imageUrl"]),
+          ),
+          );
+         }),
+        ),
+      ),
     );
   }
 }
